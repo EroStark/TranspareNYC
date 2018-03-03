cfb55f4d727bd79dc5edd9f2ec49e9a71ee60a9d
cesarinapaula <cesarinapaula@ac.c4q.nyc>
2018-03-03 17:12:50 -0500

    updated Search Component

--- transparenyc/src/Search.js
+++ transparenyc/src/Search.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-//Basic skeleton for Search Page
-//To do as of Saturday, 4:05pm: render additional appropriate information in list.
+//new state: resultsFound - initial state of search; keywordSubmitted - renders on the page after the search.
 class Search extends React.Component{
     constructor(){
         super();
         this.state={
             keywordQueryInput:'',
-            results: []
+            resultsFound: false,
+            results: [],
+            keywordSubmitted: ''
         };
     }
 
@@ -16,44 +17,53 @@ class Search extends React.Component{
         this.setState({
             keywordQueryInput: event.target.value
         })
-        console.log(this.state.keywordQueryInput);
     }
 
 //uses keywordQueryInput value to fetch query from Federal Stimulus Data API
 //stores response in this.results...for now.
     handleSubmit = event => {
         const { keywordQueryInput,results } = this.state;
-            fetch("https://data.cityofnewyork.us/resource/9haj-uwpr.json?$q=" + keywordQueryInput )
+            fetch("https://data.cityofnewyork.us/resource/9haj-uwpr.json?$q=" + keywordQueryInput + '&$limit=10')
             .then(response => response.json())
             .then(obj=>{
                 console.log(obj);
                 this.setState({
-                    results: obj
+                    resultsFound: true,
+                    results: obj,
+                    keywordSubmitted: keywordQueryInput,
+                    keywordQueryInput: ''
                 })
                 console.log(results);
+                console.log(this.state.keywordSubmitted);
             })
             .catch((error)=> {
                     console.log(error);
                 });
         };
     
-//In process: description, input field, submit button. Need to properly create component
-//to render values in a list. Additional information will be added to results.
     render(){
-        const { results }= this.state;
-        console.log("state in the render:", this.state)
+        const { results, resultsFound, keywordSubmitted, keywordQueryInput }= this.state;
+        const resultsLength = results.length;
+        const message = (resultsFound === false) ? '' : `Results for '${keywordSubmitted}', ${resultsLength} projects.`;
+
         return(
             <div>
                 <h1>Search By Keyword</h1>
-                <h3>To search by keyword, please type in a word, and click the "Submit" button.</h3>
-                <input type='text' placeholder='Please type in your keyword here' onInput={this.handleInput} />
+                <h4>To search by keyword, please type in a word, and click the "Submit" button.</h4>
+                <input type='text' placeholder='Please type in your keyword here' value={keywordQueryInput} onInput={this.handleInput} />
                 <button onClick={this.handleSubmit}>Submit</button>
-            <ol>
-                {results.map(list=>{
-                   return <li>{list.payment_recipient}</li>
-                })}
-            </ol>
-            </div>
+                <h3>{message}</h3>
+                <ol>
+                    {results.map(list=>{
+                        return <li key={list.payment_id}><p><b>Organization</b>: {list.payment_recipient}<br/> 
+                                 <b>Category</b>: {list.funding_category}<br/>
+                                 <b>Funding Amount</b>: ${list.payment_value}<br/>
+                                 <b>Project Name</b>: {list.project_name}<br/>
+                                 <b>Project Description</b>: {list.project_description}   
+                              </p></li>
+                            })}
+                </ol>
+                </div>
             
         )
     }
