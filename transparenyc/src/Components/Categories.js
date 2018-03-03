793b51f5b768fc1d9249b8d7dfd0226b9ad69869
ryanomor <omoruyi.ryan@gmail.com>
2018-03-03 17:40:02 -0500

    working on Categories component and rendering agencies

--- transparenyc/src/Components/Categories.js
+++ transparenyc/src/Components/Categories.js
@@ -8,30 +8,46 @@ class Categories extends React.Component {
     super(props)
 
     this.state = {
-      // agency: this.props.routeParams.agency,
-      agency: 'Education',
       organizations: []
     }
   }
 
-  componentDidMount(){
-     let path = //will be axios query
-     axios.get(path)
-     .then(response =>{
+  componentDidRecieveProps() {
+    let { organizations } = this.state;
+    let path = `$where=funding_category='${this.props.match.params.category}'&$group=award_lead_city_agency&$select=award_lead_city_agency`;
+     axios.get("https://data.cityofnewyork.us/resource/9haj-uwpr.json?" + path)
+     .then(response => {
+       console.log(response.data)
+       response.data.forEach(agency => { organizations.push(agency.award_lead_city_agency) });
        this.setState({
-         organizations: response.data
+         organizations: organizations
+       })
+     })
+  }
+  
+  componentWillReceiveProps() {
+    let { organizations } = this.state;
+    let path = `$where=funding_category='${this.props.match.params.category}'&$group=award_lead_city_agency&$select=award_lead_city_agency`;
+     axios.get("https://data.cityofnewyork.us/resource/9haj-uwpr.json?" + path)
+     .then(response => {
+       console.log(response.data)
+       response.data.forEach(agency => { organizations.push(agency.award_lead_city_agency) });
+       this.setState({
+         organizations: organizations
        })
      })
   }
 
   render(){
-    const { agency, organizations } = this.state
-    console.log(this.props);
+    const { organizations } = this.state
     return(
       <div>
       <h1>{this.props.match.params.category}</h1>
       {organizations.map(org =>(
-        <ProjectList name={org.award_lead_city_agency}/>
+        <p>
+          {org}
+        </p>
+        // <ProjectList name={org.award_lead_city_agency}/>
       ))}
       </div>
     )
