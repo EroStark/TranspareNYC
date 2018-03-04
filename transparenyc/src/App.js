cb2aea05f210c8af1c40033894fb55bc221b8ee5
cesarinapaula <cesarinapaula@ac.c4q.nyc>
2018-03-03 21:12:13 -0500

    changes

--- transparenyc/src/App.js
+++ transparenyc/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Link } from "react-router-dom";
+import Search from './Search'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav>
+          <Link to="/search">Search</Link>
+        </nav>
+        <Switch>
+          <Route path='/search' component={Search} />
+        </Switch>
       </div>
     );
   }
