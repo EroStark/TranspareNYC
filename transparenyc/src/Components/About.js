7b94b9de6ad768d26ba60029e718612f2013c38b
cesarinapaula <cesarinapaula@ac.c4q.nyc>
2018-03-04 22:16:18 -0500

    Updated About Component with contact information LinkedIn Github

--- transparenyc/src/Components/About.js
+++ transparenyc/src/Components/About.js
@@ -3,21 +3,24 @@ import React from 'react';
 const About = props => {
     return (
         <div>
-            <div className="blurb-team-roles">
-                <h2>Team TranspareNYC - Team Roles</h2>
-                <p>Cesarina: Quality Assurance</p>
-                <p>Eion: Tech Lead</p>
-                <p>Gregory: Product Manager</p>
-                <p>Ryan: Design Lead</p>
-                <p>Xavier: Demo Lead</p>
-            </div>
             <div className="blurb-team-contacts">
-                <h2>Team TranspareNYC - LinkedIn Profiles</h2>
-                <p><a href="https://www.linkedin.com/in/cesarina-paula/" target="_blank">Cesarina Paula</a></p>
-                <p><a href="https://www.linkedin.com/in/eion-contaste/" target="_blank">Eion Contaste</a></p>
-                <p><a href="https://www.linkedin.com/in/gregdavisdeveloper/" target="_blank">Gregory Davis</a></p>
-                <p><a href="https://www.linkedin.com/in/ryan-omoruyi" target="_blank">Ryan Omoruyi</a></p>
-                <p><a href="https://www.linkedin.com/in/xavier-james-munroe/" target="_blank">Xavier Munroe</a></p>
+                <h2>Team TranspareNYC</h2>
+
+                <p><b>Quality Assurance:</b> Cesarina Paula <span>
+                    <a href="https://www.linkedin.com/in/cesarina-paula/"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>{''}
+                    <a href="https://github.com/cesarinapaula"><i class="fa fa-github" aria-hidden="true"></i></a></span></p>
+                <p><b>Technical Lead:</b> Eion Contaste <span>
+                    <a href="https://www.linkedin.com/in/eion-contaste/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                    <a href="https://github.com/EroStark"><i class="fa fa-github" aria-hidden="true"></i></a></span></p>
+                <p><b>Product Manager:</b> Gregory Davis <span>
+                    <a href="https://www.linkedin.com/in/gregdavisdeveloper/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                    <a href="https://github.com/davisgreg1"><i class="fa fa-github" aria-hidden="true"></i></a></span></p>
+                <p><b>Design Lead:</b> Ryan Omoruyi <span>
+                    <a href="https://www.linkedin.com/in/ryan-omoruyi" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                    <a href="https://github.com/ryanomor"><i class="fa fa-github" aria-hidden="true"></i></a></span></p>
+                <p><b>Demo Lead:</b> Xavier Munroe <span>
+                    <a href="https://www.linkedin.com/in/xavier-james-munroe/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+                    <a href="https://github.com/XavierC4Q"><i class="fa fa-github" aria-hidden="true"></i></a></span></p>
             </div>
             
 
